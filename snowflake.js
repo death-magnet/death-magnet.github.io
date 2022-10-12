df4c02ad3bdab8af9a2de0daf1e6a3ad2989ba49
death-magnet <66160915+death-magnet@users.noreply.github.com>
2022-10-12 22:04:09 +0000

    Update snowflake.js

--- snowflake.js
+++ snowflake.js
@@ -12,7 +12,7 @@ window.addEventListener('load', function () {
     var grayscale = (value << 16) | (value << 8) | value;
     var color = '#' + grayscale.toString(16);
     ctx.strokeStyle = color;
-    ctx.lineWidth = randomNumber(8, 20);
+    ctx.lineWidth = Math.round(randomNumber(4, 12));
     ctx.lineCap = randomNumber(0, 1) ? 'round' : 'square';
     ctx.shadowColor = 'rgba(0,0,0,0.7)';
     ctx.shadowOffsetX = 10;
@@ -55,6 +55,7 @@ window.addEventListener('load', function () {
 
     function drawFractal()
     { 
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.save();
         ctx.translate(canvas.width * 0.5, canvas.height * 0.5);
         for (let i = 0; i < sides; i++)
@@ -66,4 +67,19 @@ window.addEventListener('load', function () {
         ctx.restore();
     }
     drawFractal();
+
+    function randomizeFractal()
+    {
+        size = canvas.width < canvas.height ? canvas.width * 0.24 : canvas.height * 0.24;
+        sides = Math.round(randomNumber(3, 9));
+        maxLevel = Math.round(randomNumber(2, 4));
+        scale = randomNumber(0.3, 0.6);
+        spread = randomNumber(0.4, 1.2);
+        branches = randomNumber(1, 4);
+    }
+    reloadButton.addEventListener('click', function ()
+    { 
+        randomizeFractal();
+        drawFractal();
+    });
 });
