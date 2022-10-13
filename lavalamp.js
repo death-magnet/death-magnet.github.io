dc3cffb5b786446dd06c2a274cbcd1bce09cd059
death-magnet <Death_Magnet81@proton.me>
2022-10-13 20:18:53 +0800

    cleaned up code, small tweaks

--- lavalamp.js
+++ lavalamp.js
@@ -5,10 +5,6 @@ window.addEventListener('load', function () {
     canvas.height = window.innerHeight;
     ctx.fillStyle = 'teal';
     const numBlobs = Math.floor(canvas.width < canvas.height ? canvas.width * 0.5 : canvas.height * 0.5);
-    var dt;
-    var target_fps = 60;
-    let lastTick = performance.now();
-    var nowish;
     class Ball {
         //ball stuff
         constructor(effect) {
@@ -21,15 +17,13 @@ window.addEventListener('load', function () {
             this.gravity = 0.05;
             this.gravitySpeed = 0;
             this.offset = 2.4;
-
         }
-        accelerate(newSpeed) {
         
+        accelerate(newSpeed) {
             this.gravity += newSpeed * 0.1;
         }
-        update(dt) {
 
-
+        update() {
             if (this.x < this.radius || this.x > this.effect.width - this.radius) this.speedX *= -1;
             //if (this.y < this.radius * 0.5 || this.y > this.effect.height - this.radius / 2) this.speedY *= -1;
             if (this.y > this.effect.height - this.radius)
@@ -38,10 +32,10 @@ window.addEventListener('load', function () {
                 this.y = this.radius;
 
             this.gravitySpeed += this.speedY + this.gravity;
-            this.x += this.speedX;// * dt * target_fps * 0.0005
-            this.y += this.gravitySpeed * 0.35; //* dt * target_fps * 0.0005;
+            this.x += this.speedX;
+            this.y += this.gravitySpeed * 0.35
             if (this.y - this.radius > this.effect.height - this.radius) {
-                this.accelerate(-0.06);
+                this.accelerate(-0.02);
                 if (this.gravity < 0)
                     this.radius = Math.random() * Math.floor(canvas.width < canvas.height ? canvas.width * 0.1 : canvas.height * 0.1) + 30;
             }
@@ -52,10 +46,9 @@ window.addEventListener('load', function () {
                 this.gravitySpeed = this.radius * this.offset;
             if (this.gravitySpeed < -this.radius * this.offset)
                 this.gravitySpeed = -this.radius * this.offset;
-            console.log("Speed: " + this.speedY + " Gravity: " + this.gravitySpeed.toFixed(4) + " Y pos: " + this.y.toFixed(2) + " Window height " + this.effect.height + " Radius: " + this.radius.toFixed(2));
+            //console.log("Speed: " + this.speedY + " Gravity: " + this.gravitySpeed.toFixed(4) + " Y pos: " + this.y.toFixed(2) + " Window height " + this.effect.height + " Radius: " + this.radius.toFixed(2));
             if (this.y < this.effect.height - (this.radius * this.offset) && this.y > (this.radius * this.offset) && this.gravitySpeed < 0)
                 this.gravitySpeed = -this.radius * 0.1;
-
         }
         
         draw(context) {
@@ -71,7 +64,6 @@ window.addEventListener('load', function () {
             this.width = width;
             this.height = height;
             this.metaballsArray = [];
-            
         }
         
         init(numberOfBalls) {
@@ -81,34 +73,26 @@ window.addEventListener('load', function () {
             }
         }
         
-        update(dt) {
+        update() {
             //update stuff
-            this.metaballsArray.forEach(metaball => metaball.update(dt));
-
+            this.metaballsArray.forEach(metaball => metaball.update());
         }
         
         draw(context) {
             //draw stuff
             this.metaballsArray.forEach(metaball => metaball.draw(context));
         }
-
     }
-
 
     const effect = new MetaballsEffect(canvas.width, canvas.height);
     effect.init(numBlobs);
 
     function animate() {
         //animate stuff
-        nowish = performance.now();
-        dt = nowish - lastTick;
-        lastTick = nowish;
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        effect.update(dt);
+        effect.update();
         effect.draw(ctx);
-        
         window.requestAnimationFrame(animate);
     }
-
     animate();
 });
