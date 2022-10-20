6a90d0d472d439f4368b25226c89f0535e8a018f
death-magnet <Death_Magnet81@proton.me>
2022-10-21 04:04:14 +0800

    drawing tool kinda working

--- draw.js
+++ draw.js
@@ -7,20 +7,23 @@ window.addEventListener('load', function () {
 
   ctx.lineJoin = 'round';
   ctx.lineCap = 'round';
-  ctx.lineWidth = 20;
+  let brushSize = 10;
+  ctx.lineWidth = brushSize;
   ctx.strokeStyle = '#ac0000';
 
   let isDrawing = false;
   let lastX = 0;
   let lastY = 0;
+  let colorInput = document.querySelector('#color');
 
+    const slider_brushSize = document.getElementById('brushSize');
+    const label_brushSize = document.querySelector('[for="brushSize"]');
   function draw(e) {
     // stop the function if they are not mouse down
     if (!isDrawing) return;
     //listen for mouse move event
     ctx.beginPath();
     ctx.moveTo(lastX, lastY);
-    console.log(e);
     if (e.offsetX == undefined) {
       ctx.lineTo(e.changedTouches[e.changedTouches.length - 1].pageX, e.changedTouches[e.changedTouches.length - 1].pageY);
       ctx.stroke();[lastX, lastY] = [e.changedTouches[e.changedTouches.length - 1].pageX, e.changedTouches[e.changedTouches.length - 1].pageY];
@@ -48,5 +51,23 @@ window.addEventListener('load', function () {
   canvas.addEventListener('touchmove', draw);
   canvas.addEventListener('touchend', () => isDrawing = false);
   canvas.addEventListener('touchleave', () => isDrawing = false);
-    
+  
+    slider_brushSize.addEventListener('change', function (e)
+    { 
+        brushSize = e.target.value;
+        ctx.lineWidth = brushSize;
+
+        updateSliders();
+    });
+    colorInput.addEventListener('input', () =>{
+    let color = colorInput.value;
+    // document.body.style.backgroundColor = color;
+    ctx.strokeStyle = color;
+});
+    function updateSliders()
+    { 
+        slider_brushSize.value = brushSize;
+        label_brushSize.innerText = 'Size: ' + Number(brushSize);
+    }
+    updateSliders();
 });
